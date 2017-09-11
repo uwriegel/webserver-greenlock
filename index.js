51118399ea86140846e0e2cd13a546c6092ecbd3
Uwe Riegel <uwriegel@gmail.com>
2017-09-11 19:17:54 +0200

    staging OK

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-this.express = require('express');  
-this.app = this.express();
+'use strict';
 
-var http = require('https');  
-var fs = require('fs');
+// returns an instance of node-greenlock with additional helper methods 
+const lex = require('greenlock-express').create({
+    // set to https://acme-v01.api.letsencrypt.org/directory in production 
+    server: 'staging'
 
-var sslPath = '/etc/letsencrypt/live/riegel.selfhost.eu/';
+    // If you wish to replace the default plugins, you may do so here 
+    // 
+    , challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '~/tmp/acme-challenges' }) }
+    , store: require('le-store-certbot').create({ webrootPath: '~/tmp/acme-challenges' })
 
-var options = {  
-    key: fs.readFileSync(sslPath + 'privkey.pem'),
-    cert: fs.readFileSync(sslPath + 'fullchain.pem')
-};
+    // You probably wouldn't need to replace the default sni handler 
+    // See https://git.daplie.com/Daplie/le-sni-auto if you think you do 
+    //, sni: require('le-sni-auto').create({}) 
 
-this.server = http.createServer(options, this.app);  
-this.io = require('socket.io').listen(this.server);  
-this.server.listen(443);  
+    , approveDomains: approveDomains
+})
+
+function approveDomains(opts, certs, cb) {
+    // This is where you check your database and associated 
+    // email addresses with domains and agreements and such 
+   
+   
+    // The domains being approved for the first time are listed in opts.domains 
+    // Certs being renewed are listed in certs.altnames 
+    if (certs) 
+        opts.domains = certs.altnames
+    
+    else
+        opts.email = 'uwriegel@gmail.com'
+        opts.agreeTos = true
+    
+    // NOTE: you can also change other options such as `challengeType` and `challenge` 
+    // opts.challengeType = 'http-01'; 
+    // opts.challenge = require('le-challenge-fs').create({}); 
+   
+    cb(null, { options: opts, certs: certs })
+}
+
+// handles acme-challenge and redirects to https 
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+    console.log("Listening for ACME http-01 challenges on", this.address())
+})
+
+var express = require('express')
+var app = express()
+app.use('/affe', function (req, res) {
+    res.end('Hello, World!')
+})
+
+app.use(express.static('web'))
+   
+// handles your app 
+require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+    console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address())
+})
